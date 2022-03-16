0ab58edd2d5e76e7e0bae40d3e9ac831c511e53b
Brandon <brandon@frostinfotech.com>
2022-03-16 12:43:50 -0500

    WIP to add parallel processing

--- RecountCodeChallenge/RecountCodeChallenge/Main.cs
+++ RecountCodeChallenge/RecountCodeChallenge/Main.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Collections.Concurrent;
 
 namespace RecountCodeChallenge
 {
@@ -38,33 +39,36 @@ namespace RecountCodeChallenge
 		{
 			this.rootUrl = rootUrl;
 		}
-
 		public async Task Crawl()
 		{
 			List<string> visitedPages = new List<string>();
 			List<string> phoneNumbers = new List<string>();
-			Stack<string> pagesToVisit = new Stack<string>();
-
-
-			pagesToVisit.Push(this.rootUrl);
-
+			var pagesToVisit = new ConcurrentQueue<string>();
 			var httpClient = new HttpClient();
 
-			while (pagesToVisit.Count > 0)
+			pagesToVisit.Enqueue(this.rootUrl);
+
+			Action consumer = async () =>
 			{
-				var currentUrl = pagesToVisit.Pop();
+				string currentUrl;
+				while (pagesToVisit.TryDequeue(out currentUrl))
+				{
+					string html = "";
+					html = await httpClient.GetStringAsync(currentUrl); ;
+					//var html = await httpClient.GetStringAsync(currentUrl);
+					var htmlDoc = new HtmlDocument();
+					htmlDoc.LoadHtml(html);
+					var links = htmlDoc.DocumentNode.Descendants("a").Select(s => s.GetAttributeValue("href", ""));
 
-				var html = await httpClient.GetStringAsync(currentUrl);
-				var htmlDoc = new HtmlDocument();
-				htmlDoc.LoadHtml(html);
-				var links = htmlDoc.DocumentNode.Descendants("a").Select(s => s.GetAttributeValue("href", ""));
+					visitedPages.Add(currentUrl);
+					phoneNumbers.AddRange(GetPhoneNumbers(html));
 
-				visitedPages.Add(currentUrl);
-				phoneNumbers.AddRange(GetPhoneNumbers(html));
-
-				links.Where(s => !visitedPages.Contains(s))
-					.ForEach(s => pagesToVisit.Push(s));
-			}
+					links.Where(s => !visitedPages.Contains(s))
+						.ForEach(s => pagesToVisit.Enqueue(s));
+				}
+			};
+			do { Parallel.Invoke(consumer); }
+			while (pagesToVisit.Any());
 
 			Logger.Info("FOUND THE FOLLOWING PHONE NUMBERS:");
 			phoneNumbers.ForEach(s => Logger.Info(s));
